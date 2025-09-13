e1e0a7158bf1e4ed4f6a8fd1d37ae332ceb6e933
Avi choudhary <avichoudhary3113@gmail.com>
2025-09-13 11:23:53 +0530

    Update next.config.js removed export

--- next.config.js
+++ next.config.js
@@ -12,7 +12,6 @@ const nextConfig = {
         ? "https://api.topplaced.com"
         : "http://localhost:5000",
   },
-  output: "export", // ✅ enables static export
 };
 
 module.exports = nextConfig;
