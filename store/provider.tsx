8de1894b4d835d00e56375b0c40cee5931e39572
Anubhav-panwar <anubhavpanwar66@gmail.com>
2025-09-17 22:00:37 +0530

    OTP

--- store/provider.tsx
+++ store/provider.tsx
@@ -3,8 +3,19 @@
 import { Provider } from 'react-redux';
 import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
+import { useEffect, useState } from 'react';
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
